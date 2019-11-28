fce6da4aaa01db687e14c27c127d984979dc8173
Dominique Crétel <dc.geslain@free.fr>
2019-11-28 17:03:32 +0100

    Rename test class

---
+++ src/test/java/com/example/demo4/Demo4ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo4;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+class Demo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
